90ac74453d4e77add02654ccaea6256c8c9f5a8c
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2020-07-02 21:49:24 +0900

    add run_pandocker.py wrapper

---
+++ bin/run_pandocker.py
@@ -0,0 +1,15 @@
+import subprocess as sp
+import argparse
+
+DOCKER = "cd {}; pwd; docker run --rm -it -v$PWD:/workdir k4zuki/pandocker-alpine:2.8"
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    args = parser.parse_args()
+    sp.run(DOCKER.format(args.input), shell=True)
+
+
+if __name__ == '__main__':
+    main()
