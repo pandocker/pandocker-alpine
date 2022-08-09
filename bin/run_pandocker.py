6604d8086d35251db82a306ffdc506cc8b798924
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2022-08-09 09:57:57 +0900

    update run_pandocker.py - apply default value for image_version option

--- bin/run_pandocker.py
+++ bin/run_pandocker.py
@@ -7,7 +7,7 @@ DOCKER = "cd {}; pwd; docker run --rm -it -v$PWD:/workdir k4zuki/pandocker-alpin
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input")
-    parser.add_argument("image_version")
+    parser.add_argument("image_version", default="2.19")
     args = parser.parse_args()
     sp.run(DOCKER.format(args.input, args.image_version), shell=True)
 
